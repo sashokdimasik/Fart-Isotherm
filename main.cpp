f74b13e474fed38e30b4b1b9b28b8f358af798d7
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-27 16:56:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
-/*******************************
- * Author:       Platonov A.A. *
- * Group:        ПИ-231        *
- * Variant:      14            *
- * Date:         20.09.2023    *
- * Project name: Fart Isotherm *
- *******************************/
+/***********************************************
+ * Author:       Platonov A.A.                 *
+ * Group:        ПИ-231                        *
+ * Variant:      14                            *
+ * Date:         20.09.2023                    *
+ * Project name: Fart Isotherm                 *
+ * OnlineGDB: https://onlinegdb.com/Ft4um0vHol *
+ ***********************************************/
 
 #include <iostream>
 #include <iomanip>
 #include <math.h>
 
-#define A 1.786
+using namespace std;
 
 int main() {
-  std::cout << std::fixed << std::setprecision(3);
-  
   double volume[10] = {0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 2, 2.5, 3};
+  double work;
+
+  cout << "Enter work: ";
+  cin >> work;
+  
+  cout << fixed << setprecision(3);
   
   for (int i = 0; i < 10; i++) {
-    std::cout << "V = " << volume[i] << " P1 = " << A / volume[i] << " P2 = " << A / pow(volume[i], 1.2) << '\n';
+    cout << "V = " << volume[i] << " P1 = " << work / volume[i] << " P2 = " << work / pow(volume[i], 1.2) << '\n';
   }
   
   return 0;
